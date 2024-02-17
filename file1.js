57aa80150a706b3d4b21b9826a500d9260cf660d
Taner ÜZÜM <uzumtaner.official@gmail.com>
2024-02-17 16:20:41 +0300

    Problem 6

--- file1.js
+++ file1.js
@@ -119,4 +119,32 @@
 //
 
 // ÖDEV 6
-// TESTING
+
+// import Koa from 'koa';
+// import Router from 'koa-router';
+
+// const app = new Koa();
+// const router = new Router();
+
+// router.get('/', ctx => {
+//     ctx.body = '<h1>KOA | Index sayfasına hoşgeldiniz</h1>';
+// });
+
+// router.get('/hakkimda', ctx => {
+//     ctx.body = '<h1>Hakkımda sayfasına hoşgeldiniz</h1>';
+// });
+
+// router.get('/iletisim', ctx => {
+//     ctx.body = '<h1>İletişim sayfasına hoşgeldiniz</h1>';
+// });
+
+// app.use(router.routes()).use(router.allowedMethods());
+
+// const port = 3000;
+// app.listen(port, () => console.log(`Sunucu http://localhost:${port} adresinde çalışıyor.`));
+
+//
+//
+//
+
+// ÖDEV 7
