ec5559c3609d37bed4446014ddb04166bedd5314
Taner ÜZÜM <uzumtaner.official@gmail.com>
2024-02-18 15:59:52 +0300

    CleanBlog Project in PCAT folder

---
+++ PCAT/CleanBlog/index.js
@@ -0,0 +1,20 @@
+
+// npm run start -> with nodemon
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+
+    const blog = {
+        id: 1,
+        title: "Blog title",
+        description: "Blog description"
+    }
+    res.send(blog)
+})
+
+const port = 4000;
+app.listen(port, () => {
+    console.log(`Sunucu http://localhost:${port} portunda başlatıldı..`);
+});
