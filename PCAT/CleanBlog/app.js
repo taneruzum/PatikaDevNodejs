28f0a6be1bca557a938156c6ea13f27b676fd7a5
Taner ÜZÜM <uzumtaner.official@gmail.com>
2024-02-18 19:16:44 +0300

    Static files uploaded.

---
+++ PCAT/CleanBlog/app.js
@@ -0,0 +1,25 @@
+
+// npm run start -> with nodemon
+// const express = require('express');
+
+// const app = express();
+// app.use(express.static('public'));
+
+
+
+// app.get('/', (req, res) => {
+
+//     const blog = {
+//         id: 1,
+//         title: "Blog title",
+//         description: "Blog description"
+//     }
+//     res.send(blog)
+// })
+
+// const port = 4000;
+// app.listen(port, () => {
+//     console.log(`Sunucu http://localhost:${port} portunda başlatıldı..`);
+// });
+
+
--- PCAT/app.js
+++ PCAT/app.js
@@ -1,18 +1,49 @@
+// const express = require('express');
+
+// const app = express();
+
+// app.get('/', (req, res) => {
+
+//   const photo = {
+//     id: 1,
+//     name: "Photo Name",
+//     description: "Photo description"
+//   }
+//   res.send(photo)
+// })
+
+// const port = 3000;
+// app.listen(port, () => {
+//   console.log(`Sunucu http://localhost:${port} portunda başlatıldı..`);
+// });
+// ------------------------------------------------------------------------------------------------
+
+
+// npm run start -> with nodemon
 const express = require('express');
+const path = require('path');
 
 const app = express();
+app.use(express.static('public'));
+
+// app.get('/', (req, res) => {
+
+//   const blog = {
+//     id: 1,
+//     title: "Blog title",
+//     description: "Blog description"
+//   }
+//   res.send(blog)
+// })
 
 app.get('/', (req, res) => {
 
-  const photo = {
-    id: 1,
-    name: "Photo Name",
-    description: "Photo description"
-  }
-  res.send(photo)
+  res.sendFile(path.resolve(__dirname, 'temp/index.html'))
 })
 
-const port = 3000;
+
+
+const port = 4000;
 app.listen(port, () => {
   console.log(`Sunucu http://localhost:${port} portunda başlatıldı..`);
 });
