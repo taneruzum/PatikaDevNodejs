5d736f5b36bbb24c7d9d205e5b31f42afbfac51c
Taner ÜZÜM <uzumtaner.official@gmail.com>
2024-02-18 20:46:24 +0300

    ejs trials

--- PCAT/app.js
+++ PCAT/app.js
@@ -22,8 +22,10 @@
 // npm run start -> with nodemon
 const express = require('express');
 const path = require('path');
+const ejs = require('ejs');
 
 const app = express();
+app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
 // app.get('/', (req, res) => {
@@ -38,7 +40,17 @@ app.use(express.static('public'));
 
 app.get('/', (req, res) => {
 
-  res.sendFile(path.resolve(__dirname, 'temp/index.html'))
+  //res.sendFile(path.resolve(__dirname, 'views/index.ejs'))
+  res.render("index")
+
+})
+app.get('/about', (req, res) => {
+  res.render("about")
+
+})
+app.get('/add', (req, res) => {
+  res.render("add")
+
 })
 
 
